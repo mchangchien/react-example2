4c1688110a3a1b870684d9de8bfa8fd5ea649725
mchangchien <matt.changchien@microsoft.com>
2025-03-23 19:18:13 +0800

    first

---
+++ api/src/functions/GetRoles.js
@@ -0,0 +1,24 @@
+const { app } = require('@azure/functions');
+
+
+
+app.http('GetRoles', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const user = request.body || {};
+        const roles = ["ttt","xxx"];
+ 
+        return {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+            body: { roles }
+        };
+        
+
+    }
+});
+
+
